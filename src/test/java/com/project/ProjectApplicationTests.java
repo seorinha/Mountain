47709a0629464fbb57ae20e32b9625be9493c866
학원 <testtest@gmail.com>
2023-11-21 21:37:43 +0900

    Mountain #1 테스트

--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -2,16 +2,34 @@ package com.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 
 @SpringBootTest  //spring boot를 기동시킨다 
 class ProjectApplicationTests {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Test //비어있는지 체크 
+	void 검사_테스트() {
+		//String str = null;
+		String str = "";
+		if (ObjectUtils.isEmpty(str)) { //null or ""
+			logger.info("####str은 null또는 비어있다.####");
+		}
+		
+		//List<Integer> list = null;
+		List<Integer> list = new ArrayList<>();
+		if (ObjectUtils.isEmpty(list)) { //null or []
+			logger.info("$$$ list는 null이거나 비어있다");
+		}
+	}
 	
 	@Test
 	void contextLoads() {
