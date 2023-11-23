7f4905294f1bd260abbe19f3f9c9f284d49618b5
seorin <lyn6.6@kakao.com>
2023-11-23 14:53:16 +0900

    Mountain #1

--- src/main/java/com/project/test/TestController.java
+++ src/main/java/com/project/test/TestController.java
@@ -15,37 +15,37 @@ import com.project.user.repository.UserRepository;
 @Controller
 public class TestController {
 
-	@Autowired
-	private UserRepository userMapper;
-	
-	//1. string + response body
-	@ResponseBody
-	   @RequestMapping("/test1")
-	   public String helloWorld() {
-	       return "Hello world!";
-	   }
-	 
-	//2. map + response body -> json
-	@ResponseBody
-	@GetMapping("/test2")
-	public Map<String, Object> test2() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("a", 1);
-		map.put("b", 2);
-		return map;
-	}
-	
-	//3. jsp 확인 -> html
-	@GetMapping("/test3")
-	public String test3() {
-		return "test/test";
-	}
-	
-	//4. db연동 확인 -> json
-	@ResponseBody
-	@GetMapping("/test4")
-	public List<Map<String, Object>> test4() {
-		return userMapper.selectUserList();
-	}
+//	@Autowired
+//	private UserRepository userMapper;
+//	
+//	//1. string + response body
+//	@ResponseBody
+//	   @RequestMapping("/test1")
+//	   public String helloWorld() {
+//	       return "Hello world!";
+//	   }
+//	 
+//	//2. map + response body -> json
+//	@ResponseBody
+//	@GetMapping("/test2")
+//	public Map<String, Object> test2() {
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("a", 1);
+//		map.put("b", 2);
+//		return map;
+//	}
+//	
+//	//3. jsp 확인 -> html
+//	@GetMapping("/test3")
+//	public String test3() {
+//		return "test/test";
+//	}
+//	
+//	//4. db연동 확인 -> json
+//	@ResponseBody
+//	@GetMapping("/test4")
+//	public List<Map<String, Object>> test4() {
+//		return userMapper.selectUserList();
+//	}
 	 
 }
