2a55b3512d834156cb510c62d2f44c7eb7e43aeb
seorin <lyn6.6@kakao.com>
2023-12-05 01:52:34 +0900

    Mountain #1 테스트

--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -11,10 +11,45 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.ObjectUtils;
 
+import lombok.Getter;
+
 @SpringBootTest  //spring boot를 기동시킨다 
 class ProjectApplicationTests {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Getter
+	public enum Status {
+		//열거형 정의
+		Y(1, true), //생성자 호출
+		N(0, false); //생성자 호출 ()가 있으면 무조건 생성자를 호출해야한다
+		
+		//enum 항목에 정의된 필드
+		private int value1;
+		private boolean value2;
+		
+		//생성자: 필드에 값을 세팅하는 역할
+		Status(int value1, boolean value2) {
+			this.value1 = value1;
+			this.value2 = value2;
+		}
+		
+	}
+	
+	@Test
+	void enum_테스트1() {
+		//given - 준비
+		Status status = Status.Y;
+		
+		//when - 실행
+		int value1 = status.getValue1();
+		boolean value2 = status.isValue2();
+				
+		//then - 검증
+		assertEquals(status, Status.Y);
+		assertEquals(value1, 1);
+		assertEquals(value2, true);
+	}
 	
 	@Test //비어있는지 체크 
 	void 검사_테스트() {
@@ -51,5 +86,6 @@ class ProjectApplicationTests {
 //	void 유저추가테스트() {
 //		userBO.addUser("test", "aaaa", "test", "testest");
 //	}
+	
 	
 }
